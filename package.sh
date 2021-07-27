78d8b1e1cf36c123f062b4d077d10b8befdae2c5
Jun-Ki, Hong <dylanhong920509@gmail.com>
2021-07-28 00:42:20 +0900

    add script of needed packages

---
+++ package.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo apt install libpython2.7 libncursesw5 libncursesw5-dev neovim build-essential cmake gdb lldb llvm clang libncurses5 libncurses5-dev neovim gcc-multilib g++-multilib bison flex device-tree-compiler openssh-server openssh-client minicom
+
